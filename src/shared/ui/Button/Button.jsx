91caf72c31cd7437bd9c2a5d7305935593055127
Пользователь <nikitamir558@gmail.com>
2025-08-03 00:25:04 +0300

    feat(button): добавлены propTypes и поддержка type

--- src/shared/ui/Button/Button.jsx
+++ src/shared/ui/Button/Button.jsx
@@ -1,12 +1,39 @@
 import styles from "./Button.module.scss";
-export const Button = ({ children, onClick, className = "", ...props }) => {
+import React from "react";
+import PropTypes from "prop-types";
+import cn from "classnames";
+export const Button = ({
+  children,
+  onClick,
+  variant = "primary",
+  type = "button",
+  disabled,
+  ...props
+}) => {
   return (
     <button
+      type={type}
       onClick={onClick}
-      className={`${styles.button} ${className}`}
+      className={cn(styles.button, styles[variant])}
+      disabled={disabled}
       {...props}
     >
       {children}
     </button>
   );
+};
+
+Button.propsTypes = {
+  children: PropTypes.node.isRequired,
+  variant: PropTypes.oneOf(["primary", "secondary", "danger"]),
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
+};
+
+Button.defaultProps = {
+  variant: "primary",
+  onClick: () => {},
+  disabled: false,
+  type: "button",
 };
