232b0b7372ea26c837a6ffdc60776d60bb77a286
Alexander Mamontov <alexander2555@yandex.ru>
2025-08-02 20:26:40 +0200

    feat: Добавлен переключатель цветовой темы - добавлен компонент ThemeSwitcher - рефакторинг css-переменных (_colors.scss) - вывод компонента в Navbar.tsx - добавлены стили в App.module.css

--- src/widgets/navbar/ui/Navbar.jsx
+++ src/widgets/navbar/ui/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Link, useLocation } from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
+import {ThemeSwitcher} from '../../../widgets/theme-switcher';
 import styles from './Navbar.module.scss';
 
 const Navbar = () => {
@@ -7,20 +8,25 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
-        <Link to="/" className={styles.logo}>
+        <Link to='/' className={styles.logo}>
           React Team
         </Link>
-        
+
         <div className={styles.nav}>
-          <Link 
-            to="/" 
-            className={`${styles.navLink} ${location.pathname === '/' ? styles.active : ''}`}
+          <ThemeSwitcher />
+          <Link
+            to='/'
+            className={`${styles.navLink} ${
+              location.pathname === '/' ? styles.active : ''
+            }`}
           >
             Команда
           </Link>
-          <Link 
-            to="/favorites" 
-            className={`${styles.navLink} ${location.pathname === '/favorites' ? styles.active : ''}`}
+          <Link
+            to='/favorites'
+            className={`${styles.navLink} ${
+              location.pathname === '/favorites' ? styles.active : ''
+            }`}
           >
             Избранное
           </Link>
