570c11ca6084f7e7fe5ceb984657ee4376a6c121
Пользователь <nikitamir558@gmail.com>
2025-08-03 17:42:44 +0300

    finalcomponent Progress

--- src/entities/member/ui/MemberCard.jsx
+++ src/entities/member/ui/MemberCard.jsx
@@ -1,5 +1,5 @@
-import PropTypes from "prop-types";
-import styles from "./MemberCard.module.scss";
+import PropTypes from 'prop-types';
+import styles from './MemberCard.module.scss';
 
 const MemberCard = ({ member }) => {
   return (
